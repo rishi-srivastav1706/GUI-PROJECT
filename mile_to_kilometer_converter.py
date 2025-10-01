93c86d083fe39bb938985d8413934274f4fdc237
Rishi Srivastav <rishiaiweb@gmail.com>
2025-10-01 22:31:00 +0530

    Update mile_to_kilometer_converter.py

--- mile_to_kilometer_converter.py
+++ mile_to_kilometer_converter.py
@@ -3,7 +3,7 @@ from tkinter import *
 def miles_to_km():
     miles = float(miles_input.get())
     km = miles * 1.689
-    kilometer_label.config(text = f"{km}")
+    kilometer_result_label.config(text = f"{km}")
 
 
 window = Tk()
@@ -20,6 +20,9 @@ miles_label.grid(column=2, row=0)
 
 is_equal_label = Label(text="is equal to")
 is_equal_label.grid(column=0 , row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
 
 kilometer_label = Label(text="km")
 kilometer_label.grid(column=2 , row=1)
@@ -42,3 +45,4 @@ calculate_button.grid(column=1 , row=2)
 
 
 window.mainloop()
+
